90724197e9655a0d83664de253f5351340830fc4
Achnaton Rabelo <achnaton.rabelo@gmail.com>
2023-11-03 15:41:45 -0400

    Estilização das saídas com rich.console

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ de alguns recursos do Python, tais como:
 AUTOR: Achnaton Pessoa Rabelo
 """   
 
-import os as comando
+import os as cm
 
-from rich import print
+from rich.console import Console
 
-comando.system('cls')
+console = Console()
+
+cm.system('cls')
 
 MENU = '''
 [bold green][1][/bold green]: -> Criar Arquivo
@@ -27,75 +29,76 @@ MENU = '''
 [bold green][0][/bold green]: -> Encerrar programa
 '''
 
-OP = '-195376'
+OP = ''
 
 opcoes_menu = ('1', '2', '3', '4', '5', '6', '7', '8', '0')
 
 while OP != '0':
     try:
-        print(MENU)
-        OP = input('Informe a opção desejada: ')
+        console.print(MENU)
+        OP = console.input('Informe a opção desejada: ')
         if OP in opcoes_menu:
             #  op = int(op_s)
             if OP == '1':
-                comando.system('cls')
-                print('########### CRIANDO ARQUIVOS ###########')
-                fname = input('Informe o nome do arquivo: ')
+                cm.system('cls')
+                console.print('########### CRIANDO ARQUIVOS ###########')
+                fname = console.input('Informe o nome do arquivo: ')
                 file_name = f"{fname}.txt"
 
                 with open(file_name, 'x', encoding='utf-8') as f1:
-                    print(f'Arquivo {file_name.upper()} criado com sucesso')
+                    console.print(f'Arquivo {file_name.upper()} criado com sucesso')
             elif OP == '2':
-                comando.system('cls')
-                print('########### LENDO ARQUIVOS ###########')
-                fname = input('Informe o nome do arquivo: ')
+                cm.system('cls')
+                console.print('########### LENDO ARQUIVOS ###########')
+                fname = console.input('Informe o nome do arquivo: ')
                 file_name = f"{fname}.txt"
 
                 with open(file_name, 'r', encoding='utf-8') as f1:
-                    print(f1.read())
+                    console.print(f1.read())
             elif OP == '3':
-                comando.system('cls')
-                print('########### EDITANDO ARQUIVOS ###########')
-                fname = input('Informe o nome do arquivo: ')
+                cm.system('cls')
+                console.print('########### EDITANDO ARQUIVOS ###########')
+                fname = console.input('Informe o nome do arquivo: ')
                 file_name = f"{fname}.txt"
 
-                texto = input('Digite abaixo:\n')
+                texto = console.input('Digite abaixo:\n')
                 with open(file_name, 'a', encoding='utf-8') as f1:
                     f1.write(texto + '\n')
             elif OP == '4':
-                comando.system('cls')
-                print('########### DELETANDO ARQUIVOS ###########')
-                fname = input('Informe o arquivo a deletar: ')
+                cm.system('cls')
+                console.print('########### DELETANDO ARQUIVOS ###########')
+                fname = console.input('Informe o arquivo a deletar: ')
                 fname = f'{fname}.txt'
 
-                if comando.path.exists(fname):
-                    comando.remove(fname)
-                    print('Arquivo deletado com sucesso!')
+                if cm.path.exists(fname):
+                    cm.remove(fname)
+                    console.print('Arquivo deletado com sucesso!')
                 else:
-                    print('O nome de arquivo informado não existe!')
+                    console.print('O nome de arquivo informado não existe!')
             elif OP == '5':
-                comando.system('cls')
+                cm.system('cls')
             elif OP == '6':
-                comando.system('cls')
-                dir_atual = comando.getcwd()
-                print('PASTA ATUAL:', dir_atual)
-                dir_atual = comando.listdir()
+                cm.system('cls')
+                dir_atual = cm.getcwd()
+                console.print(f'[blue]PASTA ATUAL:[/blue] [underline]{dir_atual}[/underline]')
+                dir_atual = cm.listdir()
                 for i in dir_atual:
-                    print(i)
+                    console.print(i)
             elif OP == '7':
-                comando.system('cls')
-                print('########### ALTERANDO PASTA ATUAL ###########')
-                destino = comando.chdir(input('Mudar para: '))
-                print('PASTA ATUAL:', comando.getcwd())
-            elif OP == '8':
-                comando.system('cls')
-                corrente = comando.getcwd()
-                print('DIRETÓRIO CORRENTE:', corrente)
+                cm.system('cls')
+                console.print('########### ALTERANDO PASTA ATUAL ###########')
+                destino = cm.chdir(console.input('Mudar para: '))
+                console.print('PASTA ATUAL:', cm.getcwd())
+            elif OP == '8': # Localização atual
+                cm.system('cls')
+                corrente = cm.getcwd()
+                console.print(f'[blue]DIRETÓRIO CORRENTE:[/blue] [underline]{corrente}[/underline]')
         else:
-            print('Opção inválida, tente novamente!')
+            cm.system('cls')
+            console.print('[bold red]Opção inválida, tente novamente![bold red]')
     except FileExistsError:
-        print('Arquivo já existe!')
+        console.print('Arquivo já existe!')
     except FileNotFoundError:
-        print('Arquivo não encontrado')
+        console.print('Arquivo não encontrado')
 
-comando.system('cls')
+cm.system('cls')
